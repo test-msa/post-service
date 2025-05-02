5b1a4e3d73dd36c987c9240002deacecc9ca1f5a
legoChoi <chlx159@gmail.com>
2025-05-03 01:41:38 +0900

    init

---
+++ src/main/java/test/msa/post/PostServiceApplication.java
@@ -0,0 +1,15 @@
+package test.msa.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
